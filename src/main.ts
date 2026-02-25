34f9b62f2771381d2a879d0bd7baadbe919e3b68
Alap user <diak@petrik.hu>
2026-02-25 11:59:48 +0100

    formsubmit()

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ let lista: Bejegyzes[] = [];
 const kiiras = document.getElementById("kiiras") as HTMLElement;
 
 function megjelenit() {
+  kiiras.innerHTML = "";
   lista.forEach(blog => {
     const article = document.createElement("article") as HTMLElement;
     const h2 = document.createElement("h2") as HTMLHeadingElement;
@@ -19,11 +20,36 @@ function megjelenit() {
   })
 }
 
+function formSubmit(e: SubmitEvent) {
+  e.preventDefault();
+  try 
+  {
+    const cim = document.getElementById("cim") as HTMLInputElement;
+    const tartalom = document.getElementById("content") as HTMLInputElement; 
+    const color = document.getElementById("color") as HTMLInputElement;
+    
+    // console.log(cim.value,tartalom.value,color.value);
+    lista.push(new Bejegyzes(cim.value,tartalom.value,color.value));
+    cim.value = "";
+    tartalom.value = "";
+    color.value = "";
+    megjelenit();
+  }
+  catch (err)
+  {
+    if( err instanceof Error)
+    {
+
+    }
+  }
+}
+
 function init() {
   lista.push(new Bejegyzes("cim1", "ez tartalom", "#F78A72"));
   lista.push(new Bejegyzes("cim2", "ez a masik tartalom", "#72F78D"));
-  console.log(lista);
+  // console.log(lista);
   megjelenit();
 }
 
 document.addEventListener("DOMContentLoaded",init);
+document.getElementById("form")?.addEventListener("submit",formSubmit);
